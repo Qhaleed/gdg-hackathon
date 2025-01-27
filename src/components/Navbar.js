145f185afe20947a5a8c6baf3c8f2bcfa0f17ea4
Qhaleed <reslykadiri@gmail.com>
2025-01-27 13:45:21 +0800

    UPDATE

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ export function Navbar() {
         <nav className="nav">
             <div className="nav-brand">
                 <img
-                    src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Screenshot%20(265)-TBZWWbNmCltXE664UD5fGy3OTSclyO.png"
+                    src="./heading1.png"
+
                     alt="Google Developer Groups Logo"
                     className="nav-logo"
                 />
-                <span className="nav-title">Google Developer Groups</span>
             </div>
             <div className="nav-links">
                 <a href="#" className="nav-link">About GDG</a>
